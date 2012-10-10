e421210f1e81205638fdb6a040ab96a280952f23
André Pereira <apereira@apereira.internal.hq.grupopie.com>
2012-10-10 14:24:46 +0100

    Core updated

--- src/core/corevisual.h
+++ src/core/corevisual.h
@@ -2,13 +2,8 @@
 #define COREVISUAL_H
 #include <GL/glut.h>
 
-<<<<<<< HEAD
 #define  KEV_MOUSE_LEFT  GLUT_LEFT_BUTTON
 #define  KEV_MOUSE_RIGHT GLUT_RIGHT_BUTTON
-=======
-#define GE_MOUSE_LEFT GLUT_LEFT_BUTTON
-#define GE_MOUSE_RIGHT GLUT_RIGHT_BUTTON
->>>>>>> 65d16911d9d1f0545a047daf7f514baa1b13dae7
 
 class CoreVisual {
  private:
